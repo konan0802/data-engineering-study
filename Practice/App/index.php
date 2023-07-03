e3d126e95d0bf77af0bee21f7642ea9395fc14d6
konan0802 <konanforbis@gmail.com>
2023-07-03 13:05:38 +0000

    fix: loginファイルを別出し

--- Practice/App/index.php
+++ Practice/App/index.php
@@ -1,34 +1,10 @@
 <?php
 
-require_once './config.php';
-
 // URLパラメータからルートパスを取得する
 $url = $_SERVER['REQUEST_URI'];
 
-if (preg_match('/\.php$/', $url)) {
-    header('Location: /');
-    exit;
-}
-
-// ログイン画面の場合は遷移
-if ($url === "/login") {
-    include "./view/login.php";
-    exit;
-}
-
-// 未ログインの場合はログイン画面にリダイレクト
-$uid = $_COOKIE["uid"];
-if (is_null($uid)) {
-    header('Location: /login');
-    exit;
-}
-
-// ユーザーが存在しない場合はログイン画面にリダイレクト
-$user = $db->fetchUserFromUid($uid);
-if (!$user) {
-    header('Location: /login');
-    exit;
-}
+require_once './config.php';
+require_once './login_check.php';
 
 var_dump("ok");
 exit;
---
+++ Practice/App/login_check.php
@@ -0,0 +1,27 @@
+<?php
+
+// phpファイルへのアクセスはトップにリダイレクト
+if (preg_match('/\.php$/', $url)) {
+    header('Location: /');
+    exit;
+}
+
+// ログイン画面の場合は遷移
+if ($url === "/login") {
+    include "./view/login.php";
+    exit;
+}
+
+// 未ログインの場合はログイン画面にリダイレクト
+$uid = $_COOKIE["uid"];
+if (is_null($uid)) {
+    header('Location: /login');
+    exit;
+}
+
+// ユーザーが存在しない場合はログイン画面にリダイレクト
+$user = $db->fetchUserFromUid($uid);
+if (!$user) {
+    header('Location: /login');
+    exit;
+}
