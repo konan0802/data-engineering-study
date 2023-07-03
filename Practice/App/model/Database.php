bce7de25023cfc67561cd75434f26f964c17d43a
konan0802 <konanforbis@gmail.com>
2023-07-03 23:38:05 +0000

    fix: docの追加

--- Practice/App/model/Database.php
+++ Practice/App/model/Database.php
@@ -1,8 +1,19 @@
 <?php
 
+/**
+ * データベースクラス
+ */
 class Database {
+    /** @var PDO データベース接続オブジェクト */
     private $conn;
 
+    /**
+     * Databaseクラスの新しいインスタンスを作成します。
+     *
+     * @param string $dsn データベースのDSN
+     * @param string $username データベースのユーザー名
+     * @param string $password データベースのパスワード
+     */
     public function __construct($dsn, $username, $password) {
 
         $this->conn = new PDO($dsn, $username, $password);
@@ -13,7 +24,10 @@ class Database {
     }
 
     /**
-     * 
+     * ユーザーIDに基づいてユーザーを取得します。
+     *
+     * @param int $uid ユーザーID
+     * @return array ユーザーの連想配列
      */
     public function fetchUserFromUid(int $uid) {
         $sql = "SELECT *
@@ -28,7 +42,11 @@ class Database {
     }
 
     /**
-     * 
+     * ユーザー名とパスワードに基づいてユーザーを取得します。
+     *
+     * @param string $username ユーザー名
+     * @param string $password パスワード
+     * @return array ユーザーの連想配列
      */
     public function fetchUserFromUserName(string $username, string $password) {
         $sql = "SELECT *
@@ -44,6 +62,16 @@ class Database {
         return $result;
     }
 
+    /**
+     * ユーザーを作成します。
+     *
+     * @param string $username ユーザー名
+     * @param string $email メールアドレス
+     * @param string $password パスワード
+     * @param int $gender 性別
+     * @param int $age 年齢
+     * @param string $prefecture 都道府県
+     */
     public function createUser(
         string $username,
         string $email,
@@ -57,13 +85,20 @@ class Database {
         $stmt = $this->conn->prepare($sql);
         $stmt->bindParam(1, $username, PDO::PARAM_STR);
         $stmt->bindParam(2, $email, PDO::PARAM_STR);
-        $stmt->bindParam(3, $password, PDO::PARAM_STR);
+        $stmt->bindParam
+
+(3, $password, PDO::PARAM_STR);
         $stmt->bindParam(4, $gender, PDO::PARAM_INT);
         $stmt->bindParam(5, $age, PDO::PARAM_INT);
         $stmt->bindParam(6, $prefecture, PDO::PARAM_STR);
         $stmt->execute();
     }
     
+    /**
+     * すべての商品を取得します。
+     *
+     * @return array 商品の連想配列
+     */
     public function fetchAllProducts() {
         $sql = "SELECT * FROM Products";
         $stmt = $this->conn->query($sql);
@@ -71,6 +106,13 @@ class Database {
         return $products;
     }
 
+    /**
+     * カートに商品を追加します。
+     *
+     * @param int $userId ユーザーID
+     * @param int $productId 商品ID
+     * @param int $quantity 数量
+     */
     public function addToCart(int $userId, int $productId, int $quantity)
     {
         // 既存のカート内の数量を更新するために、同じユーザーと商品の組み合わせのレコードを検索
@@ -86,6 +128,13 @@ class Database {
         }
     }
     
+    /**
+     * ユーザーと商品に基づいてカート内のアイテムを取得します。
+     *
+     * @param int $userId ユーザーID
+     * @param int $productId 商品ID
+     * @return array カートアイテムの連想配列
+     */
     private function fetchCartByUserAndProduct(int $userId, int $productId)
     {
         $sql = "SELECT * FROM Cart WHERE user_id = ? AND product_id = ?";
@@ -96,6 +145,12 @@ class Database {
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
     
+    /**
+     * カートの数量を更新します。
+     *
+     * @param int $cartId カートID
+     * @param int $newQuantity 新しい数量
+     */
     private function updateCartQuantity(int $cartId, int $newQuantity)
     {
         $sql = "UPDATE Cart SET quantity = ? WHERE id = ?";
@@ -105,6 +160,15 @@ class Database {
         $stmt->execute();
     }
     
+    /**
+     * カートに新しいレコードを挿入します。
+     *
+     * @param int $userId ユーザーID
+     * @param int $productId 商品ID
+     * @param
+
+ int $quantity 数量
+     */
     private function insertCartRecord(int $userId, int $productId, int $quantity)
     {
         $sql = "INSERT INTO Cart (user_id, product_id, quantity)
@@ -116,6 +180,12 @@ class Database {
         $stmt->execute();
     }
     
+    /**
+     * ユーザーのカート内のアイテムを取得します。
+     *
+     * @param int $userId ユーザーID
+     * @return array カートアイテムの連想配列
+     */
     public function fetchCartItems(int $userId) {
         $sql = "SELECT p.name, p.price, c.quantity
                 FROM Cart c
@@ -126,7 +196,4 @@ class Database {
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-
-
-
 }
