c42018e7b998f2ffc2012ead84a48c21c1b619ba
konan0802 <konanforbis@gmail.com>
2023-07-10 20:25:03 +0900

    完了画面

--- Practice/App/view/completed.php
+++ Practice/App/view/completed.php
@@ -3,6 +3,9 @@
 // ステータスを完了に更新するSQLクエリ
 $db->updateCartStatus($user['uid']);
 
+// カート内の商品情報を取得するSQLクエリ
+$cartItems = $db->fetchCartItems($user['uid']);
+
 ?>
 
 <!DOCTYPE html>
@@ -13,11 +16,15 @@ $db->updateCartStatus($user['uid']);
     <script>
         setTimeout(function(){
             window.location.href = "/";
-        }, 3000); // 3秒後にルートページにリダイレクト
+        }, 2000); // 3秒後にルートページにリダイレクト
     </script>
 </head>
 <body>
-    <h1>購入が完了しました</h1>
-    <p>ありがとうございました。</p>
+    <?php if (empty($cartItems)): ?>
+        <h1>カートは空です</h1>
+    <?php else: ?>
+        <h1>購入が完了しました</h1>
+        <p>ありがとうございました。</p>
+    <?php endif; ?>
 </body>
 </html>
